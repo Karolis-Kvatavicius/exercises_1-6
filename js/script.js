ea38cd669a5dbd2e7caa36a864efdff6269210ab
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2020-12-12 08:31:26 +0200

    EXERCISE 2

--- js/script.js
+++ js/script.js
@@ -20,3 +20,27 @@ btn_green.addEventListener('click', function() {
 btn_red.addEventListener('click', function() {
     setClass(text, 'alert-danger');
 });
+
+
+// EXERCISE 2
+let larger = document.querySelector("#padidinti");
+let smaller = document.querySelector("#sumazinti");
+
+let htmlIcon = document.querySelector("#first-ico");
+
+// MAKE ICON BIGGER
+larger.addEventListener('click', function() {
+    htmlIcon.classList.toggle("fa-10x");
+    // htmlIcon.classList.add('fa-10x');
+})
+
+// MAKE ICON SMALLER
+smaller.addEventListener('click', function() {
+    htmlIcon.classList.remove("fa-5x", "fa-10x");
+})
+
+// RESET ICON SIZE
+htmlIcon.addEventListener('click', function() {
+    this.classList.remove("fa-10x");
+    this.classList.add('fa-5x');
+})
