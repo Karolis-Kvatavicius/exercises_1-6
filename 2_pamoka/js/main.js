89a046b04c9d0980ffdac42e0afee0c350334d15
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2021-01-06 15:32:14 +0200

    4 uzduotis - can add task with enter, 6 uzduotis - got rid of switch statement, 7 uzduotis - images are of consistent size

--- 2_pamoka/js/main.js
+++ 2_pamoka/js/main.js
@@ -77,6 +77,28 @@ prideti.addEventListener('click', function (e) {
     }
 })
 
+// KEYDOWN
+document.addEventListener('keydown', function (e) {
+    if (e.isComposing || e.keyCode === 229) {
+        return;
+    }
+    inputValue = task.value;
+    if (inputValue != "" && e.keyCode == 13) {
+        let listItem = document.createElement("li");
+        listItem.classList.add("list-group-item");
+        let taskText = document.createTextNode(inputValue);
+        listItem.append(taskText);
+        sarasas.append(listItem);
+
+        let uzduotys = document.querySelectorAll('.list-group-item');
+        uzduotys.forEach(i => {
+            i.addEventListener('click', e => {
+                e.target.remove();
+            })
+        })
+    }
+})
+
 //5
 let input1 = document.querySelector('#input1');
 let input2 = document.querySelector('#input2');
@@ -102,22 +124,10 @@ let paveikslelis = document.querySelector('#paveikslelis');
 let pasirinkimas = document.querySelector('#id_of_select');
 
 pasirinkimas.addEventListener('change', e => {
-    switch (e.target.value) {
-        case '0':
-            paveikslelis.style.backgroundImage = "none";
-            break;
-        case '1':
-            paveikslelis.style.backgroundImage = "url(https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80)";
-            break;
-        case '2':
-            paveikslelis.style.backgroundImage = "url(https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80)";
-            break;
-        case '3':
-            paveikslelis.style.backgroundImage = "url(https://images.unsplash.com/photo-1517468832098-4843428b6d38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)";
-            break;
-        case '4':
-            paveikslelis.style.backgroundImage = "url(https://images.unsplash.com/photo-1484313544071-4d67c88b99be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80)";
-            break;
+    if(e.target.value == '0') {
+        paveikslelis.style.backgroundImage = "none";
+    } else {
+        paveikslelis.style.backgroundImage = "url("+ e.target.value +")";
     }
 })
 
@@ -125,7 +135,7 @@ pasirinkimas.addEventListener('change', e => {
 let main_image = document.querySelector('#main-picture');
 let small_images = document.querySelectorAll('[id^="picture-"]');
 let main_picture_alt = document.querySelector('#main-picture-alt');
-main_picture_alt.innerHTML = main_image.alt; 
+main_picture_alt.innerHTML = main_image.alt;
 
 small_images.forEach(image => {
     image.addEventListener('click', e => {
@@ -145,12 +155,12 @@ small_images.forEach(image => {
 let skaiciuoti = document.querySelector('#skaiciuok');
 
 skaiciuoti.addEventListener('click', e => {
-   e.preventDefault();
-   let skaiciuokles_forma = document.forms['arbatos_skaiciuokle'];
-   let arbata = Number(skaiciuokles_forma['ivertinimas'].value)/100;
-   let rezultatas = (Number(skaiciuokles_forma['sask_suma'].value) * arbata / Number(skaiciuokles_forma['zm_skaicius'].value)).toFixed(2);
-   document.querySelector('#arbatos_suma').innerHTML = `${rezultatas}<sup>Eur</sup>`;
-   e.target.classList.add('d-none');
-   e.target.classList.remove('d-block');
-   document.querySelector('#skaic_rezultatas').classList.remove('d-none');
+    e.preventDefault();
+    let skaiciuokles_forma = document.forms['arbatos_skaiciuokle'];
+    let arbata = Number(skaiciuokles_forma['ivertinimas'].value) / 100;
+    let rezultatas = (Number(skaiciuokles_forma['sask_suma'].value) * arbata / Number(skaiciuokles_forma['zm_skaicius'].value)).toFixed(2);
+    document.querySelector('#arbatos_suma').innerHTML = `${rezultatas}<sup>Eur</sup>`;
+    e.target.classList.add('d-none');
+    e.target.classList.remove('d-block');
+    document.querySelector('#skaic_rezultatas').classList.remove('d-none');
 })
